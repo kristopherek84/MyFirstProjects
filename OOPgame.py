04c67bea10fcefb75cf3df8b75f122e1dca6326f
Jacek <jokerchocolate@gmail.com>
2019-03-06 19:05:43 +0100

    OOP

---
+++ OOPgame.py
@@ -0,0 +1,65 @@
+class Person(object):
+    pass
+
+class Hero(Person):
+    pass
+
+class Monster(Person):
+    pass
+
+class Animall(Person):
+    pass
+
+class Fish(Person):
+    pass
+
+class Shopkeeper(Person):
+    pass
+
+
+
+class Location(object):
+    pass
+
+class Death(Location):
+    pass
+
+class Forest(Location):
+    pass
+
+class Dungeon(Location):
+    pass
+
+class Lake(Location):
+    pass
+
+class Shop(Location):
+    pass
+
+
+
+class Items(object):
+    pass
+
+class Money(Items):
+    pass
+
+class Bow(Items):
+    pass
+
+class Pickaxe(Items):
+    pass
+
+class FishingRod(Items):
+    pass
+
+class Axe(Items):
+    pass
+
+class Food(Items):
+    pass
+
+
+
+class Engine(object):
+    pass
